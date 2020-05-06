b977523d22dcf3c0765889084b4aa01d564252db
xlinGithub <11510523+xlinGithub@users.noreply.github.com>
2020-05-06 15:43:34 -0400

    modified index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({hi: 'there! this is my first web app!'});
 })
 
 PORT = process.env.PORT || 5000
