cf55ef46106d20d54e2360a401a58db32e299fb6
xlinGithub <xiaochen.lin.2005@gmail.com>
2020-05-06 21:35:54 -0400

    started google oauth

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there! this is my first web app!'});
-})
+passport.use(new GoogleStrategy());
+
+// app.get('/', (req, res) => {
+//     res.send({hi: 'there! this is my first web app!'});
+// })
 
 PORT = process.env.PORT || 5000
 app.listen(PORT)
