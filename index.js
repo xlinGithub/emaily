f892ef6272de5679c9674b370a03cb494cdd63c9
xlinGithub <11510523+xlinGithub@users.noreply.github.com>
2020-05-08 13:59:58 -0400

    added google auth

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const app = express();
+const keys = require('./config/keys');
 
-// client ID 17044488995-hdstn0tuuirojhqk3amvu8nd6sh74ben.apps.googleusercontent.com
-// client secret n2atmjryE-aTEEjpKz4MfzmQ
+const app = express();
 
 app.get('/', (req, res) => {
     res.send({hi: 'there! this is my first web app!'});
-})
+});
 
-PORT = process.env.PORT || 5000
-app.listen(PORT)
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID, 
+    clientSecret: keys.googleClientSecret, 
+    callbackURL: 'google/callback'
+}, (accessToken) => {
+    console.log(accessToken);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+PORT = process.env.PORT || 5000;
+app.listen(PORT);
