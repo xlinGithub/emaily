fb67c60974297a238ef09fdec34440edb9fa6516
xlinGithub <11510523+xlinGithub@users.noreply.github.com>
2020-05-08 11:34:39 -0400

    added client id and secret

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+
+// client ID 17044488995-hdstn0tuuirojhqk3amvu8nd6sh74ben.apps.googleusercontent.com
+// client secret n2atmjryE-aTEEjpKz4MfzmQ
+
 app.get('/', (req, res) => {
     res.send({hi: 'there! this is my first web app!'});
 })
