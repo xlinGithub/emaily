9e393b1a3a30d71eff49e4b1bec13d27a2fbcf46
xlinGithub <xiaochen.lin.2005@gmail.com>
2020-05-11 21:46:39 -0400

    refactored

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
 const app = express();
+require("./routes/authRoutes")(app);
 
 app.get('/', (req, res) => {
     res.send({hi: 'there! this is my first web app!'});
 });
-
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID, 
-    clientSecret: keys.googleClientSecret, 
-    callbackURL: 'google/callback'
-}, (accessToken) => {
-    console.log(accessToken);
-}));
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,11 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID, 
+    clientSecret: keys.googleClientSecret, 
+    callbackURL: 'google/callback'
+}, (accessToken) => {
+    console.log(accessToken);
+}));
