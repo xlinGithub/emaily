4a1de3a19a187298ee452d680120b8f5e4cba644
xlinGithub <xiaochen.lin.2005@gmail.com>
2020-05-14 11:07:21 -0400

    set up production keys

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// prod.js - production keys!!!
+module.exports = {
+    googleClientID: '943555306991-t7fpv92uonr2dr27rjrf8gjtc49m9dkv.apps.googleusercontent.com17044488995-hdstn0tuuirojhqk3amvu8nd6sh74ben.apps.googleusercontent.com',
+    googleClientSecret: 'QNmFfRdTiM38VuImyjSuHEuy-aTEEjpKz4MfzmQ',
+    mongoURI: 'mongodb+srv://myuser:yogtcoEfbZDvFbrV@cluster0-f44ws.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'asdfasdf',
+}
