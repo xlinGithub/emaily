ab1a92787b52a2511c6e7c998eafa22c9a4dbe84
xlinGithub <xiaochen.lin.2005@gmail.com>
2020-05-22 16:59:44 -0400

    added redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const App = () => {
-    return(
-        <div>
-            Hi There!
-        </div>
-    )
+import Header from './Header';
+import Landing from './Landing';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
+
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    };
+    
+    render() {
+        return(
+            <div className='container'>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route path='/' component={Landing} exact/>
+                        <Route path='/surveys' component={Dashboard} exact/>
+                        <Route path='/surveys/new' component={SurveyNew} exact/>
+                    </div>
+                </BrowserRouter>
+            </div>
+    )};
 };
 
-export default App;
+export default connect(null, actions)(App);
